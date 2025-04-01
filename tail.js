8d32a07ab94a057a60f6ace5bdfa54cf01a0446a
Brendan <brendan94brown@gmail.com>
2025-03-31 21:47:22 -0700

    Fixed, FINALLY

--- tail.js
+++ tail.js
@@ -14,3 +14,7 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
