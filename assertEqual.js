86c47eb843ea1d5f5a95439cb0ed70fc1ebd900e
Brendan <brendan94brown@gmail.com>
2025-03-31 19:09:49 -0700

    Created assertEqual function, added emojis for clarity

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅Assertion Passed: ${actual} === ${expected}");
+  } else {
+    console.log("🛑Assertion Failed: ${actual} !== ${expected}");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(4, (2 + 2));
+assertEqual(3, (-3 + 6));
+assertEqual("I'm Dumb", "Hopefully not too dumb");
