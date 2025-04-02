7eb6be1262a45b6ce331d1ff741084b79a0bb32d
Brendan <brendan94brown@gmail.com>
2025-04-01 21:18:25 -0700

    Added statement to check and compare length, as well as a test result to fill out requirements

--- eqArrays.js
+++ eqArrays.js
@@ -8,6 +8,10 @@ const assertEqual = function(actual, expected) {
 
 
 const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
@@ -18,6 +22,6 @@ const eqArrays = function(array1, array2) {
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2]));
