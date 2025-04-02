d1ca1ee085c89073893f289f1fb1648193e35651
Brendan <brendan94brown@gmail.com>
2025-04-01 21:20:43 -0700

    Fixed last test result by adding second argument to fulfill requirements

--- eqArrays.js
+++ eqArrays.js
@@ -24,4 +24,4 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2]));
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
