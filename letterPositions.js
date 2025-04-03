c8d9f065384a450c5d2db2d383d5fb0bc4a5f53f
Brendan <brendan94brown@gmail.com>
2025-04-02 20:59:20 -0700

    Changed declarations in for() loop from let => const

--- letterPositions.js
+++ letterPositions.js
@@ -22,8 +22,8 @@ const assertArraysEqual = function(array1, array2) {
 const letterPositions = function(sentence) {
   const results = {};
   
-  for (let i = 0; i < sentence.length; i++) {
-    let letter = sentence[i];
+  for (const i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
 
     if (letter !== " ") {
       if (results[letter]) {
