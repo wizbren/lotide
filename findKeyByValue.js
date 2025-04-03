ea51b421c0239eaebb5240182a95cbaae8d40375
Brendan <brendan94brown@gmail.com>
2025-04-02 21:21:22 -0700

    Implemented findKeyByValue function, using assertEqual to test

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  const keys = Object.keys(object);
+
+  for (let i = 0; i < keys.length; i++) {
+    if (object[keys[i]] === value) {
+      return keys[i];
+    }
+  }
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
