303a13a688946892256b7de13f355f9d22f2db23
Brendan Brown <brendan94brown@gmail.com>
2025-04-07 16:47:56 -0700

    Relocated test cases, and added export/require dependencies for both functions

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(4, (2 + 2));
-assertEqual(3, (-3 + 6));
+module.exports = assertEqual;
