066e8a89f852cf978c7ae57f1254cc4148835cec
Brendan Brown <brendan94brown@gmail.com>
2025-04-07 17:52:43 -0700

    Removed uncalled functions, exported middle function, moved test cases over to middleTest

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -32,7 +11,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([3, 6]), []);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([3, 6, 9, 12, 15]), [9]);
-assertArraysEqual(middle([5, 10, 15, 20, 25, 30]), [15, 20]);
+module.exports = middle;
