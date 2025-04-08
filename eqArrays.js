484c3b56ce98a82bc5740119d3d81bf4da60cd52
Brendan Brown <brendan94brown@gmail.com>
2025-04-07 17:19:18 -0700

    Imported assertEqual function, exported eqArrays function, and moved test cases to eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 
 const eqArrays = function(array1, array2) {
@@ -18,10 +12,6 @@ const eqArrays = function(array1, array2) {
     }
   }
   return true;
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+module.exports = eqArrays;
