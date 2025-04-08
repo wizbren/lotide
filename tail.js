63c37f7ac3674da854283eb8321d66f141a50b64
Brendan Brown <brendan94brown@gmail.com>
2025-04-07 17:03:06 -0700

    Exported tail function, imported modules to new tailTest file, and moved cases from tail to tailTest

--- tail.js
+++ tail.js
@@ -1,20 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1);
 };
-const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
