4cc3cf96487239873b6be9e4d1d935c2003dd4e1
Brendan Brown <brendan94brown@gmail.com>
2025-04-07 19:00:26 -0700

    Added mocha and chai functionality, replaced test cases with describe/it statements

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,21 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
 
-assertArraysEqual(middle([3, 6]), []);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([3, 6, 9, 12, 15]), [9]);
-assertArraysEqual(middle([5, 10, 15, 20, 25, 30]), [15, 20]);
+describe("#middle", () => {
+  it("returns an empty array when passed [3, 6]", () =>{
+    assert.deepEqual(middle([3, 6]), []);
+  });
+
+  it("returns an empty array when passed an empty array", () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it("returns [9] when passed [3, 6, 9, 12, 15]", () => {
+    assert.deepEqual(middle([3, 6, 9, 12, 15]), [9]);
+  });
+
+  it("returns [15, 20] when passed [5, 10, 15, 20, 25, 30", () => {
+    assert.deepEqual(middle([5, 10, 15, 20, 25, 30]), [15, 20]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,28 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns an array with the elements after the first index for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+    assert.strictEqual;(result[0], "Lighthouse");
+    assert.strictEqual;(result[1], "Labs");
+  });
 
-assertEqual(tail(["test"]).length, 0);  //Test: if tail is passed an array with only one element
-assertEqual(tail([]).length, 0);        //Test: if tail is passed an empty array
+  it("doesn't modify the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+  it("returns an empty array for single element arrays", () => {
+    const result = tail(["test"]);
+    assert.strictEqual(result.length, 0);
+  });
+
+  it("returns an empty array when passed an empty array", () => {
+    const result = tail([]);
+    assert.strictEqual(result.length, 0);
+  });
+});
